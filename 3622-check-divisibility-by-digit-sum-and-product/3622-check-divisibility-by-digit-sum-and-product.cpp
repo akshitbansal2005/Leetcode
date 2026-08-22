1769d4fc432572cadec242990ae47d130ce3f3f6
Akshit Bansal <155195875+akshitbansal2005@users.noreply.github.com>
2026-08-22 21:45:51 +0530

    Time: 0 ms (100%), Space: 7.8 MB (37.42%) - LeetHub

---
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+
+
+/* 2 functions bana lege like -> prod and sum jisme
+     sum =0, and product =1 and %10 lege just because of 0,1,2,3,4,5,6,7,8,9
+        and last mei return krdege   */
+    int prod(int n){
+        int res =1;
+        while(n!=0){
+            res=res*(n%10);
+            n/=10;
+        }
+        return res;
+    }
+    int sum(int n){
+        int res =0;
+        while(n!=0){
+            res=res+(n%10);
+            n/=10;
+        }
+        return res;
+    }
+    bool checkDivisibility(int n) {
+        return n %(sum(n)+prod(n))==0;
+    }
+};
