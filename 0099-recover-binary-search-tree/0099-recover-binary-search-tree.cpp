f5e5d20907933be596c5fe4ce9766d79a4b99dbd
Akshit Bansal <155195875+akshitbansal2005@users.noreply.github.com>
2026-08-27 00:33:50 +0530

    Time: 0 ms (100%), Space: 60.7 MB (78.75%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -10,16 +10,17 @@
  * };
  */
 
-/*to ek baar question ke example ko dekh kr ye pata chal gya ki what bst really is 
+/* to ek baar question ke example ko dekh kr ye pata chal gya ki what bst really is 
   i mean ki jo main node ke right wali value of hogi vo hamesha badi hogi as compared 
   to main node and jo left value hogi vo small hogi which makes a valid bst 
 
   ahhh jo mei soch raha tha vo half he correct, jo logic mei likh raha tha usse error 
   aagya 
 
-  to iss baar ky krege ki 3 variables banege first,second, third sabko null rakh dege
-
-    */     
+  to iss baar ky krege ki 3 variables banege first,second, third sabko null rakh dege 
+  iske baad ky hoga ki agar first =null hua to first =prev element hoga and second bann 
+  jayega root elem otherwise prev ==root and in main function will swap both left and right element 
+*/     
 class Solution {
 public:
     TreeNode* first=NULL,*second=NULL, *prev=NULL;
